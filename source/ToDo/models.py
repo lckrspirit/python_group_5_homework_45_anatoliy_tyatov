16b17a00f3ff7002975a202acd8d1791b67e3353
Lckrspirit <lckrspiritus@gmail.com>
2020-07-22 22:58:54 +0600

    Startapp new app, create migrations

--- source/ToDo/models.py
+++ source/ToDo/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=100, null=False, blank=True, verbose_name='Title')
+    status = models.CharField(max_length=100, default='New', blank=True, verbose_name='Status')
+    date_perform = models.DateField(default='', verbose_name='Data')
+
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/lckrspirit/Attractor/Homework/python_group_5_homework_45_anatoliy_tyatov/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
